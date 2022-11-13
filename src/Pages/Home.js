4f9813066202d49e8a96895c8de0c17e9498b045
GoldenDavid <huylkt1@gmail.com>
2022-11-13 20:19:48 +0700

    update client

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "../Services/axiosInterceptor";
 const Home = () => {
   const navigate = useNavigate();
   const name = localStorage.getItem("name");
   const token = localStorage.getItem("token");
-
+  const [input, setInput] = useState({
+    newpassword: "",
+    confirmpassword: "",
+  });
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -12,27 +16,41 @@ const Home = () => {
     navigate("/login");
   };
 
+  const handleChangePassword = async(e) => {
+    e.preventDefault();
+    const response = await axios.post("api/auth/change-password", input, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    alert(response.data.message);
+    if (response.status === 200) {
+      handleLogout();
+    }
+  };
+
   return (
-    <section class="vh-100" style={{ backgroundColor: "#B9D6F3" }}>
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col col-xl-10">
-            <div class="card" style={{ borderRadius: "1rem" }}>
-              <div class="row g-0">
-                <div class="col-md-6 col-lg-5 d-none d-md-block">
+    <section className="vh-100" style={{ backgroundColor: "#B9D6F3" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+            <div className="card" style={{ borderRadius: "1rem" }}>
+              <div className="row g-0">
+                <div className="col-md-6 col-lg-5 d-none d-md-block">
                   <img
                     src="https://marketplace.canva.com/EAFJhI0EBag/1/0/1600w/canva-blue-cute-dog-quotes-desktop-wallpaper-g8TNhcn4QCQ.jpg"
                     alt="login form"
-                    class="img-fluid"
+                    className="img-fluid"
                     style={{ borderRadius: "1rem 0 0 1rem" }}
                   />
                 </div>
-                <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                  <div class="card-body p-4 p-lg-5 text-black">
+                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-5 text-black">
                     <h1 className="h1 text-center">Home Page</h1>
-                      <div class="d-flex align-items-center mb-3 pb-1">
+                    <form onSubmit={handleChangePassword}>
+                      <div className="d-flex align-items-center mb-3 pb-1">
                         <h2>Welcome</h2>
-                        <span class="h3 fw-bold mb-0 mx-3">{name}</span>
+                        <span className="h3 fw-bold mb-0 mx-3">{name}</span>
                         <button
                           onClick={handleLogout}
                           className="btn btn-primary"
@@ -40,6 +58,55 @@ const Home = () => {
                           Logout
                         </button>
                       </div>
+
+                      <h4
+                        className="fw-normal mb-3 pb-3"
+                        style={{ letterSpacing: "1px" }}
+                      >
+                        Change Password
+                      </h4>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          placeholder="Enter New Password"
+                          name="newpassword"
+                          type="password"
+                          className="form-control form-control-lg"
+                          value={input.newpassword}
+                          onChange={(e) =>
+                            setInput({
+                              ...input,
+                              [e.target.name]: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          placeholder="Confirm Your Password"
+                          type="password"
+                          name="confirmpassword"
+                          className="form-control form-control-lg"
+                          value={input.confirmpassword}
+                          onChange={(e) =>
+                            setInput({
+                              ...input,
+                              [e.target.name]: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+
+                      <div className="pt-1 mb-4">
+                        <button
+                          className="btn btn-dark btn-lg btn-block"
+                          type="submit"
+                        >
+                          Change Password
+                        </button>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
